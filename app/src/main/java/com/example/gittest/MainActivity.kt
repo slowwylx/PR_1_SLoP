9ad7fbbe78a1bdf5bd4ed1a6b695b6b1f8c6567a
alex <slowolfyy@gmail.com>
2024-09-20 21:12:59 +0300

    fixes some issue

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "IOS",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
