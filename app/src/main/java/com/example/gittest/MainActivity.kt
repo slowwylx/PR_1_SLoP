24c4210ea480ba1c80f584939933254c8a82aa18
alex <slowolfyy@gmail.com>
2024-09-21 12:23:19 +0300

    auth message

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "IOS",
+                        name = "Unauthorized user",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
